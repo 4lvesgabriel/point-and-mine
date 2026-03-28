62f6b8682094017b343d214118ad72abbaae7bcc
Gabriel Alves Gabriel <25028174@fecap.br>
2026-03-27 22:32:24 -0300

    codigo de prefab

--- Assets/Scripts/personagem.cs
+++ Assets/Scripts/personagem.cs
@@ -6,6 +6,9 @@ public class ClickToMove : MonoBehaviour
     private NavMeshAgent agent;
     public LayerMask groundLayer;
 
+    public GameObject clickMarkerPrefab;
+    private GameObject currentMarker;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -15,6 +18,14 @@ public class ClickToMove : MonoBehaviour
 
     void Update()
     {
+
+        if (currentMarker != null && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Destroy(currentMarker);
+            currentMarker = null;
+        }
+
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,6 +34,17 @@ public class ClickToMove : MonoBehaviour
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
             {
                 agent.SetDestination(hit.point);
+
+                if (currentMarker != null)
+                {
+                    Destroy(currentMarker);
+                }
+
+                if (clickMarkerPrefab != null)
+                {
+                    Vector3 markerPos = hit.point + Vector3.up * 0.01f; // evita z-fighting
+                    currentMarker = Instantiate(clickMarkerPrefab, markerPos, Quaternion.identity);
+                }
             }
         }
     }
