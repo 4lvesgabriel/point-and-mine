a9848cee97460c0e775d84450e77cccba1a8be88
araujolucas23 <lucas.araujo.pereira23@gmail.com>
2026-04-10 21:05:36 -0300

    funcionalidade de geração de enviroment

---
+++ Assets/Scripts/ItemQuebravel.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string nomeItem;
+    public float itemLife = 100f;
+    public Sprite iconeGrande;
+
+    private void OnMouseDown()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] itensParaGerar;
+    public int quantidadeTotal = 5;
+    
+    void Start()
+    {
+        GerarItens();    
+    }
+
+    // Update is called once per frame
+    void GerarItens()
+    {
+        GameObject[] pontos = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        List<GameObject> pontosDisponiveis = new List<GameObject>(pontos);
+
+        for (int i = 0; i < quantidadeTotal; i++)
+        {
+            if (pontosDisponiveis.Count == 0) break;
+
+            int indexPonto = Random.Range(0, pontosDisponiveis.Count);
+            int indexItem = Random.Range(0, itensParaGerar.Length);
+
+            Instantiate(itensParaGerar[indexItem], pontosDisponiveis[indexPonto].transform.position, Quaternion.identity);
+
+            pontosDisponiveis.RemoveAt(indexPonto);
+        }
+
+    }
+}
